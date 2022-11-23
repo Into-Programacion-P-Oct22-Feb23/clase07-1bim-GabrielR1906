493d99e5a8bbd59b2945efc410fbcc53faee8994
Gabriel Rojas <rgrojas4@utpl.edu.ec>
2022-11-23 15:48:16 -0500

    Semana7

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo101.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo101.java
@@ -20,7 +20,7 @@ public class Ejemplo101 {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        int limite_tabla = 12;
+        int limite_tabla;
         int contador = 1;
         int operacion;
         int tabla;
